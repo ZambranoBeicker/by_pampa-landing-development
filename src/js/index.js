50d0dfec16268491645dc44f378766754541c703
Beicker Zambrano <beicker998@gmail.com>
2020-12-11 10:28:39 -0500

    Change webpack configuration
    Update routes to compiled files to
    fix error with dev server live
    reload and add a devtool to use
    the source map

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,19 @@
 import '../sass/index.scss'
-console.log('Building the project')
+
+// import Swiper JS
+//import Swiper from 'swiper'
+//// import Swiper styles
+
+////var mySwiper = new Swiper('.swiper-container', {
+////speed: 400,
+////spaceBetween: 100,
+////})
+
+//var mySwiper = document.querySelector('.swiper-container').swiper
+
+//var $next = document.getElementById('next')
+
+//$next.addEventListener('click', () => {
+//mySwiper.slideNext()
+//})
+console.info("It's working")
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,57 @@
-const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const htmlWebpack = new htmlWebpackPlugin({
-  template: path.resolve(__dirname, "src", "index.template.html"),
-  filename: "index.html",
-  minify: "auto",
-});
+    template: path.resolve(__dirname, 'src', 'index.template.html'),
+    filename: './index.html',
+    minify: 'auto',
+})
 
 module.exports = {
-  entry: { index: path.resolve(__dirname, "src/js", "index.js") },
-  output: {
-    path: __dirname,
-    publicPath: path.join(__dirname, "src"),
-    filename: "./dist/bundle.js",
-  },
-  plugins: [
-    htmlWebpack,
-    new MiniCssExtractPlugin({
-      filename: "./dist/[name].css",
-    }),
-  ],
-  optimization: {
-    minimizer: [new OptimizeCssAssetsPlugin({})],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: "css-loader",
-          },
-          {
-            loader: "sass-loader", // compiles Sass to CSS
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        use: "url-loader",
-      },
+    entry: { index: path.resolve(__dirname, 'src/js', 'index.js') },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '.',
+        filename: 'assets/bundle.js',
+    },
+    plugins: [
+        htmlWebpack,
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css',
+        }),
     ],
-  },
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin({})],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader', // compiles Sass to CSS
+                    },
+                ],
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: 'url-loader',
+            },
+        ],
+    },
 
-  devServer: {
-    open: true,
-  },
-};
+    devtool: 'source-map',
+
+    devServer: {
+        contentBase: './dist',
+        open: true,
+    },
+}
